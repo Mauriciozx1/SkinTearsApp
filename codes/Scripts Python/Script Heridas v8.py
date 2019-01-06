026f39be3045adb1fb9382cea77fd73c82aec9b2
Elliot <c.cabreraurrejola@uandresbello.edu>
2019-01-05 22:31:42 -0300

    Create Script Heridas v5.py
    Con este codigo se logra, separar solo la herida desde la imagen inicial, se mejoran los filtros en el proceso de la imagen final.

---
+++ codes/Scripts Python/Script Heridas v8.py
@@ -0,0 +1,98 @@
+import numpy as np
+import cv2
+#from skimage import data, io, filters
+#from PIL import Image, ImageFilter
+
+
+
+#     Capturo la imagen Original
+inputImage = cv2.imread('00053.jpg')
+
+#     Genero una Imagen redimensionada
+resizedImage = cv2.resize(inputImage, (500,500))
+cv2.imshow('1.- Imagen Original redimensionada', resizedImage)
+
+
+
+#resizedImage = cv2.medianBlur(resizedImage,5) # al aumentar me redondea mucho el contorno final
+
+
+"""
+#     Filtro gaussiano para eliminar el ruido sobre imagen Redimensionada
+filtroGauss = cv2.GaussianBlur(resizedImage, (5, 5), 0)
+#     Muestro resultado de filtro gaussiano aplicado
+cv2.imshow('3.- Aplicado Filtro Gaussiano', filtroGauss)
+resizedImage = filtroGauss
+"""
+
+
+#     Convertimos la imagen redimensionada de color a gris
+grayImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
+
+#     Segmentamos la imagen
+t, binaryImage = cv2.threshold(grayImage, 0, 255, cv2.THRESH_BINARY  | cv2.THRESH_TRIANGLE)
+cv2.imshow('4.- Imagen Binaria', binaryImage)
+
+
+#     Elimiar el ruido blanco sobre las zonas negras
+#Crear un kernel de '1' de 3x3
+kernel = np.ones((3,3),np.uint8)
+#Se aplica la transformacion: Opening
+binaryImage = cv2.morphologyEx(binaryImage,cv2.MORPH_OPEN,kernel)
+
+
+
+"""
+# OJO CON ESTOS FILTROS
+binaryImage = cv2.medianBlur(binaryImage,11) # al aumentar me redondea mucho el contorno final
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+binaryImage = cv2.morphologyEx(binaryImage, cv2.MORPH_CLOSE, kernel)
+binaryImage = cv2.morphologyEx(binaryImage, cv2.MORPH_OPEN, kernel)
+"""
+
+
+#     Invierto los colores de la imagen Binaria
+binaryImageInv = cv2.bitwise_not(binaryImage)
+cv2.imshow('5.- Imagen Binaria Invertida', binaryImageInv)
+
+
+
+
+#     Eliminar el ruido negro sobre las zonas blancas
+#Crear un kernel de '1' de 3x3
+kernel = np.ones((7,7),np.uint8)
+#Se aplica la transformacion: Closing
+binaryImageInv = cv2.morphologyEx(binaryImageInv,cv2.MORPH_CLOSE,kernel)
+
+
+
+
+"""
+#Crear un kernel de '1' de 3x3
+kernel = np.ones((5,5),np.uint8)
+#Se aplica la transformacion: Dilate
+binaryImageInv = cv2.dilate(binaryImageInv,kernel,iterations = 5)
+"""
+
+
+
+
+
+
+#     Convierto imagenes de tipo uint8 a bool 
+resImgBool = resizedImage.astype(np.bool)
+invImgBool = binaryImageInv.astype(np.bool)
+
+#     Expando la dimension de la imagen Invertida y binarizada
+invImgBoolExpDim = np.expand_dims(invImgBool, axis=2)
+
+#     Realizo un producto entre la Imagen Binarizada invertida y expandida contra la imagen redimensionada
+#     Ejecutaba con error si convertia la imagen original redimensionada a np.bool
+finalimage = invImgBoolExpDim * resizedImage
+cv2.imshow('6.- Imagen Final', finalimage)
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
